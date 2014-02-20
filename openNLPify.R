148dd7081ba949f8c9e81a0250febb1760bfb67e
Kingfish Threesevens <scott.malec@gmail.com>
2014-02-20 07:42:24 -0500

    Create openNLPify.R

---
+++ openNLPify.R
@@ -0,0 +1,15 @@
+library(XML)
+library(tm)
+library(RCurl)
+library(openNLP)
+url <- "http://clover.slavic.pitt.edu/sam/propp/have_a_little_byte/magicgeese.xml"
+#url <- "http://www.maleclabs.com/Propp/Corpus.xml"
+tale <- xmlTreeParse(getURL(url), useInternal = T)
+tale
+initsit <- xmlValue(getNodeSet(tale, "//Corpus//Folktale//Move//Preparation//InitialSituation")[[1]]);
+initsit
+return <- xmlValue(getNodeSet(tale, "//Corpus//Folktale//Move//Return")[[1]])
+return
+# sentDetect(initsit, language = "en", model = NULL)
+taggedSegment <- tagPOS(initsit, language = "en", model = NULL, tagdict = NULL)
+taggedSegment
